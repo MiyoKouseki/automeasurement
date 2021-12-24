8227cd3c1f08cee28e203931c61e58d17b34ef31
Kouseki.Miyo <miyo@icrr.u-tokyo.ac.jp>
2021-12-24 11:33:49 +0900

    not use params.txt

--- run_plant.sh
+++ run_plant.sh
@@ -6,7 +6,7 @@
 # ./templates/SPECTRA_{SUS}_{STATE}_{STAGE}_{TIME}.xml
 # ./measurements/{SUS}/{PLANT,SPECTRA}/{YYYY}/{mmdd}/*{HHMM}.xml
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 5 ]; then
   echo "指定された引数は$#個です。" 1>&2
   echo "実行するには4個の引数が必要です。" 1>&2
   exit 1
@@ -16,6 +16,38 @@ SUS=$1
 STAGE=$2
 EXC=$3
 DOF=$4
+BW=$5
+
+# Fix me 
+if [[ "$DOF" = "L" ]] || [[ "$DOF" = "F0" ]]; then
+    exc_dof_num=0
+elif [[ "$DOF" = "P" ]] || [[ "$DOF" = "F1" ]]; then
+    exc_dof_num=1    
+elif [[ "$DOF" = "R" ]] || [[ "$DOF" = "F2" ]]; then
+    exc_dof_num=2    
+elif [[ "$DOF" = "T" ]] || [[ "$DOF" = "F3" ]]; then
+    exc_dof_num=3    
+elif [[ "$DOF" = "V" ]] || [[ "$DOF" = "BF" ]]; then
+    exc_dof_num=4    
+elif [[ "$DOF" = "Y" ]]; then
+    exc_dof_num=5
+elif [[ "$DOF" = "H1" ]]; then
+    exc_dof_num=6
+elif [[ "$DOF" = "H2" ]]; then
+    exc_dof_num=7
+elif [[ "$DOF" = "H3" ]]; then
+    exc_dof_num=8
+elif [[ "$DOF" = "V1" ]]; then
+    exc_dof_num=9
+elif [[ "$DOF" = "V2" ]]; then
+    exc_dof_num=10
+elif [[ "$DOF" = "V3" ]]; then
+    exc_dof_num=11   
+else
+    echo 'error'
+    exit
+fi
+  
 
 outputs_dir=./measurements/${SUS}/PLANT/`date +%Y/%m%d`
 templates_dir=./templates
@@ -61,10 +93,11 @@ DEBUG=1
 printf "\033[31;01m=== Running ${exc_name} ===\033[00m\n"
 echo "open" >tmp
 echo "restore "$template >>tmp
-echo "set Test.BW = ${bw}">>tmp
-echo "set Test.StimulusChannel[0] = ${exc_channel}">>tmp
-echo "set Test.StimulusAmplitude[0] = ${amp}">>tmp
-echo "set Test.StimulusFilter[0] = ${filter}">>tmp
+echo "set Test.BW = ${BW}">>tmp
+#echo "set Test.StimulusChannel[0] = ${exc_channel}">>tmp
+echo "set Test.StimulusActive[${exc_dof_num}] = true">>tmp
+#echo "set Test.StimulusAmplitude[0] = ${amp}">>tmp
+#echo "set Test.StimulusFilter[0] = ${filter}">>tmp
 echo "run -w" >>tmp
 echo "save "$output >> tmp
 echo "quit" >> tmp
