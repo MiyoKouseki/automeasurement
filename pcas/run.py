be589241226d1149b00922f20c264dd94e572a51
Kouseki.Miyo <miyo@icrr.u-tokyo.ac.jp>
2022-01-28 10:07:12 +0900

    TypeA以外もOVERVIEWできるようにした。

--- pcas/run.py
+++ pcas/run.py
@@ -4,7 +4,10 @@ from pcaspy import Driver, SimpleServer
 
 prefix = 'K1:'
 
-suspensions = ['ETMX','ETMY','ITMX','ITMY']
+suspensions = ['ETMX','ETMY','ITMX','ITMY',
+               'SRM','SR2','SR3','BS',
+               'PRM','PR2','PR3',
+               'MCI','MCO','MCE','IMMT1','IMMT2','OSTM','OMMT1','OMMT2']
 stages = ['IP','GAS','BF','MN','IM']
 states = ['SAFE','STANDBY','ISOLATED','DAMPED','ALIGNED']
 
