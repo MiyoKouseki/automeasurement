d9dc452fb71867c8e11cf491cede7c8f1b448c68
miyo <miyo@z400.flets-west.jp>
2022-02-14 09:56:04 +0900

    rename channel

--- lib/db.py
+++ lib/db.py
@@ -10,17 +10,18 @@ pvdb = {refnum_fmt.format(sus=sus,stg=stg,sts=sts):
 pvdb.update({refnum_fmt_sdf_dummy.format(sus=sus,stg=stg,sts=sts):
              {'type':'float','value':0} for sus,stg,sts in params})
 
-list_fmt = 'ATM-VIS_SELECT_{key1}_LIST'
-select_fmt = 'ATM-VIS_SELECT_{key1}_{key2}'
-select_bit_fmt = 'ATM-VIS_SELECT_{key1}_{key2}_BIT'
+list_fmt = 'ATM-VIS_SELECT_LIST_{key1}'
+select_fmt = 'ATM-VIS_SELECT_BUTTON_{key1}_{key2}'
+select_bit_fmt = 'ATM-VIS_SELECT_BUTTON_{key1}_{key2}_BIT'
 for key1 in ['SUS','STG','STS','REF']:
     pvdb.update({list_fmt.format(key1=key1):{'type':'str'}})
     pvdb.update({select_fmt.format(key1=key1,key2=key2):
                  {'type':'int','value':0} for key2 in key2dict[key1]})
     pvdb.update({select_bit_fmt.format(key1=key1,key2=key2):
                  {'type':'int','value':0} for key2 in key2dict[key1]})
-    if 'REF'==key1:
-        pvdb.update({'ATM-VIS_SELECT_{key1}_{key2}_VAL'.format(key1=key1,key2=key2):
+    
+    if 'REF'==key1: # fixme
+        pvdb.update({'ATM-VIS_SELECT_BUTTON_{key1}_{key2}_VAL'.format(key1=key1,key2=key2):
                      {'type':'float',
                       'value':0} for key2 in key2dict[key1]})
 
--- lib/run.py
+++ lib/run.py
@@ -10,7 +10,7 @@ from vis import suspensions,stages,states,key2dict
 prefix = 'K1:'
 
 def is_pushed(self,key1,key2):
-    return self.getParam('ATM-VIS_SELECT_{0}_{1}_BIT'.format(key1,key2))%4
+    return self.getParam('ATM-VIS_SELECT_BUTTON_{0}_{1}_BIT'.format(key1,key2))%4
 
 def get_pushed_list(self,key1):
     '''
@@ -24,7 +24,7 @@ class myDriver(Driver):
         self.tid = None
         kwargs = {'sus':['.*'], 'stg':['.*'], 'sts':['.*'], 'exc':['.*'], 'ref':['.*'], 'dof':['.*'], 'cache':True}      
         suslist,stslist,stglist,exclist,doflist,reflist = search(**kwargs)
-        [self.setParam('ATM-VIS_SELECT_REF_{0:02d}_VAL'.format(i), int(val)) for i,val in zip(range(10),reflist)]        
+        [self.setParam('ATM-VIS_SELECT_BUTTON_REF_{0:02d}_VAL'.format(i), int(val)) for i,val in zip(range(10),reflist)]        
 
     def write(self, reason, value):
         status = True
@@ -32,31 +32,31 @@ class myDriver(Driver):
         self.setParam(reason, value)
 
         if 'ATM-VIS_SEARCH' in reason:
-            suslist = self.getParam('ATM-VIS_SELECT_SUS_LIST')
-            stglist = self.getParam('ATM-VIS_SELECT_STG_LIST')
-            stslist = self.getParam('ATM-VIS_SELECT_STS_LIST')
+            suslist = self.getParam('ATM-VIS_SELECT_LIST_SUS')
+            stglist = self.getParam('ATM-VIS_SELECT_LIST_STG')
+            stslist = self.getParam('ATM-VIS_SELECT_LIST_STS')
         elif 'ATM-VIS_SELECT' in reason:
-            key1,key2 = reason.split('_')[-2:]            
-            _val = self.getParam('ATM-VIS_SELECT_{0}_{1}_BIT'.format(key1,key2))
-            self.setParam('ATM-VIS_SELECT_{0}_{1}_BIT'.format(key1,key2),_val+2)
+            key1,key2 = reason.split('_')[-2:]
+            _val = self.getParam('ATM-VIS_SELECT_BUTTON_{0}_{1}_BIT'.format(key1,key2))
+            self.setParam('ATM-VIS_SELECT_BUTTON_{0}_{1}_BIT'.format(key1,key2),_val+2)
             
             # push されているボタンのリストを取得
             suslist = get_pushed_list(self,'SUS')
             stglist = get_pushed_list(self,'STG')
             stslist = get_pushed_list(self,'STS')                        
-            self.setParam('ATM-VIS_SELECT_SUS_LIST',' '.join(suslist))            
-            self.setParam('ATM-VIS_SELECT_STG_LIST',' '.join(stglist))
-            self.setParam('ATM-VIS_SELECT_STS_LIST',' '.join(stslist))
+            self.setParam('ATM-VIS_SELECT_LIST_SUS',' '.join(suslist))            
+            self.setParam('ATM-VIS_SELECT_LIST_STG',' '.join(stglist))
+            self.setParam('ATM-VIS_SELECT_LIST_STS',' '.join(stslist))
 
-            reflist = [self.getParam('ATM-VIS_SELECT_REF_{0:02d}_VAL'.format(ref)) for ref in range(10) \
-                       if self.getParam('ATM-VIS_SELECT_REF_{0:02d}_BIT'.format(ref))%4]
-            self.setParam('ATM-VIS_SELECT_REF_LIST',' '.join(map(str,reflist)))
+            reflist = [self.getParam('ATM-VIS_SELECT_BUTTON_REF_{0:02d}_VAL'.format(ref)) for ref in range(10) \
+                       if self.getParam('ATM-VIS_SELECT_BUTTON_REF_{0:02d}_BIT'.format(ref))%4]
+            self.setParam('ATM-VIS_SELECT_LIST_REF',' '.join(map(str,reflist)))
             
             kwargs = {'sus':suslist, 'stg':stglist, 'sts':stslist, 'exc':['.*'], 'ref':['.*'], 'dof':['.*'], 'cache':True}            
             suslist,stslist,stglist,exclist,doflist,reflist = search(**kwargs)
             #print(suslist,stslist,stglist,exclist,doflist,reflist)
-            [self.setParam('ATM-VIS_SELECT_REF_{0:02d}_VAL'.format(i), int(val)) for i,val in zip(range(10),[0]*10)]            
-            [self.setParam('ATM-VIS_SELECT_REF_{0:02d}_VAL'.format(i), int(val)) for i,val in zip(range(10),reflist)]            
+            [self.setParam('ATM-VIS_SELECT_BUTTON_REF_{0:02d}_VAL'.format(i), int(val)) for i,val in zip(range(10),[0]*10)]            
+            [self.setParam('ATM-VIS_SELECT_BUTTON_REF_{0:02d}_VAL'.format(i), int(val)) for i,val in zip(range(10),reflist)]            
         else:
             pass
         
