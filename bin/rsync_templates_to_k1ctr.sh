75d0e7a7962a7d845d1641b4be68519e732ef17d
miyo <miyo@z400.flets-west.jp>
2022-02-18 09:50:52 +0900

    local から remote への同期はしない。

--- bin/rsync_templates_to_k1ctr.sh
+++ bin/rsync_templates_to_k1ctr.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-mkdir -p ../templates
-
-rsync --delete -ave \
-      ../templates/ \
-      ssh k1ctr:/kagra/Dropbox/Measurements/VIS/TEMPLATES
+# つかわないほうがいい。
+#rsync --delete -ave \
+#      ../templates/ \
+#      ssh k1ctr:/kagra/Dropbox/Measurements/VIS/TEMPLATES
