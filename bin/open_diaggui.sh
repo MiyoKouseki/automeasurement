44e11be6001dd95e32012ae2e1c3ff7679507986
Kouseki.Miyo <miyo@icrr.u-tokyo.ac.jp>
2022-02-25 12:08:26 +0900

    add open_diag button

---
+++ bin/open_diaggui.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source /kagra/Dropbox/Subsystems/VIS/Scripts/automeasurement/settings
+
+num=$1
+
+sus=`caget -t K1:ATM-VIS_ANS_${num}_SUS`
+stg=`caget -t K1:ATM-VIS_ANS_${num}_STG`
+sts=`caget -t K1:ATM-VIS_ANS_${num}_STS`
+exc=`caget -t K1:ATM-VIS_ANS_${num}_EXC`
+dofs=`caget -t K1:ATM-VIS_ANS_${num}_DOF`
+ref=`caget -t K1:ATM-VIS_ANS_${num}_REF`
+
+yyyy=${ref:0:4}
+mm=${ref:4:2}
+
+echo $dofs
+read -p "Choose dofs:" dof
+if [[ "$dofs" == *${dof}* ]]; then
+    fname=${plants_dir}/${sus}/${yyyy}/${mm}/PLANT_${sus}_${sts}_${stg}_${exc}_${dof}_${ref}.xml
+else
+    echo "Error $dof"
+    exit 1;
+fi
+
+if [ -f $fname ]; then  
+    diaggui $fname
+fi
+
